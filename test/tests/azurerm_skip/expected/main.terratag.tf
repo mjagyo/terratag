862d3722264d47c002426e96e0672c064fa991de
Mark Joseph Jagyo <mark.jagyo@inchcape.com>
2024-12-04 22:52:56 +0800

    Fix azurerm_skip

--- test/tests/azurerm_skip/expected/main.terratag.tf
+++ test/tests/azurerm_skip/expected/main.terratag.tf
@@ -48,4 +48,3 @@ locals {
   terratag_added_main_azurerm_public_ip       = {"env0_environment_id"="40907eff-cf7c-419a-8694-e1c6bf1d1168","env0_project_id"="43fd4ff1-8d37-4d9d-ac97-295bd850bf94"}
   terratag_added_main_azurerm_virtual_network = {"env0_environment_id"="40907eff-cf7c-419a-8694-e1c6bf1d1168","env0_project_id"="43fd4ff1-8d37-4d9d-ac97-295bd850bf94"}
 }
-
